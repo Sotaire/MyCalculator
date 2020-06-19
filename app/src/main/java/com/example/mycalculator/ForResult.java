87b02157bb0b1285d02f5af2ea7ff9dc8bbaec11
Sotaire <islamov-tilek@mail.ru>
2020-06-19 23:14:12 +0600

    chooser

--- app/src/main/java/com/example/mycalculator/ForResult.java
+++ app/src/main/java/com/example/mycalculator/ForResult.java
@@ -11,7 +11,7 @@ import android.widget.Toast;
 
 public class ForResult extends AppCompatActivity {
 
-    public static final  String RESULT_KEY = "result_key";
+    public static final String RESULT_KEY = "result_key";
     public static String saved;
 
     @Override
@@ -24,32 +24,32 @@ public class ForResult extends AppCompatActivity {
         btnCalculator.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(ForResult.this,MainActivity.class);
+                Intent intent = new Intent(ForResult.this, MainActivity.class);
                 startActivity(intent);
             }
         });
-        getData();
-        Button btnresult = findViewById(R.id.sendText);
-        btnresult.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent3 = new Intent();
-                intent3.putExtra(intent3.EXTRA_TEXT,saved);
-                intent3.setAction(Intent.ACTION_SEND);
-                intent3.setType("text/plain");
-
-                if (intent3.resolveActivity(getPackageManager()) != null){
-                    startActivity(intent3);
-                }
+         getData();
+         Button btnresult = findViewById(R.id.sendText);
+         btnresult.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 share(saved);
+           }
+         });
+          }
+        private void getData () {
+            Intent intent2 = getIntent();
+            if (intent2 != null) {
+                TextView btnGetResult = findViewById(R.id.getResult);
+                saved = intent2.getStringExtra(RESULT_KEY);
+                btnGetResult.setText(btnGetResult.getText().toString() + " " + saved);
             }
-        });
-    }
-    private void getData(){
-        Intent intent2 =  getIntent();
-        if (intent2 != null){
-            TextView btnGetResult = findViewById(R.id.getResult);
-            saved = intent2.getStringExtra(RESULT_KEY);
-            btnGetResult.setText(btnGetResult.getText().toString() + " " + saved);
         }
-    }
-}
+            public void share (String text){
+                final Intent intent = new Intent();
+                intent.setAction(intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(intent.EXTRA_TEXT, text);
+                startActivity(intent.createChooser(intent, saved));
+            }
+        }
